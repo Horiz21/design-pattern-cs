0f75ee3830e17a49457d5413259ceee7975b9da9
Horiz21 <htl.me@outlook.com>
2023-09-11 00:24:05 +0800

    1. Strategy - Caculator

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Design_Pattern.Strategy_Pattern;
+
+internal class Program {
+	private static void Main(string[] args) {
+		int id;
+		if (args.Length == 0) {
+			Console.WriteLine("1. Strategy - Caculator");
+			Console.Write("Please input pattern ID: ");
+			while (!int.TryParse(Console.ReadLine(), out id)) {
+				Console.Write("Wrong ID format! Retry: ");
+			}
+		}
+		else {
+			id = int.Parse(args[0]);
+		}
+		switch (id) {
+			case 1:
+				Caculator.SingleRun();
+				break;
+			default:
+				Console.WriteLine("Unknown pattern!");
+				break;
+		}
+	}
+}
---
+++ Strategy Pattern/Caculator.cs
@@ -0,0 +1,49 @@
+﻿namespace Design_Pattern.Strategy_Pattern {
+	// 策略（接口），策略接口的内容与具体执行的操作有关，它其中有一个未被实现的方法等待具体的、同一个算法族的不同实际策略来实现
+	public interface ICaculateStrategy {
+		public int DoCaculate(int num1, int num2);
+	}
+
+	// 实际策略：加法
+	public class AddCaculate : ICaculateStrategy {
+		public int DoCaculate(int num1, int num2) {
+			Console.WriteLine("ADD");
+			return num1 + num2;
+		}
+	}
+
+	// 实际策略：减法。加法、减法等所有实现同一个策略接口的构成一个算法族，也就是所有策略的集合。
+	public class SubtractCaculate : ICaculateStrategy {
+		public int DoCaculate(int num1, int num2) {
+			Console.WriteLine("SUBTRACT");
+			return num1 - num2;
+		}
+	}
+
+	// 环境
+	public class CaculatorEnvironment {
+		public ICaculateStrategy strategy;
+		public CaculatorEnvironment(ICaculateStrategy strategy) {
+			Console.WriteLine("Strategy Set!");
+			this.strategy = strategy;
+		}
+		public void SetStrategy(ICaculateStrategy strategy) {
+			Console.WriteLine("Strategy Change!");
+			this.strategy = strategy;
+		}
+		public int DoCaculate(int num1, int num2) {
+			Console.WriteLine("Do Caculate");
+			return strategy.DoCaculate(num1, num2);
+		}
+	}
+
+	// 主函数
+	public class Caculator {
+		public static void SingleRun() {
+			CaculatorEnvironment caculator = new(new AddCaculate());  // 创建计算器环境，初始设置其策略为加法
+			Console.WriteLine(caculator.DoCaculate(1, 3));  // 实施策略
+			caculator.SetStrategy(new SubtractCaculate());  // 更新原来计算器环境的策略为减法
+			Console.WriteLine(caculator.DoCaculate(1, 3));  // 实施策略
+		}
+	}
+}
